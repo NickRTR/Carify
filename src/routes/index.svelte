08bf3a146fdf722f8d964be7a34640211c4acd8e
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-03-06 15:31:05 +0100

    Added origins

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { de } from "$lib/origins.js";
+</script>
+
+<body>
+    <p>{de.BL.title}</p>
+    <p>{de.BL.state}</p>
+</body>
