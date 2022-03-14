59e16fc7d00b7ce69b9f5ef2d9f16d94d903e2da
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-03-14 07:35:38 +0100

    New: API instrocutions at .../api

---
+++ src/routes/api/index.svelte
@@ -0,0 +1,25 @@
+<main>
+    <h2>API</h2>
+
+    <h3>API URL:</h3>
+    <p style="text-decoration: underline;">https://carify.vercel.app/api/(country)-(search).json</p>
+
+    <ul>
+        <li>country: D / A</li>
+        <li>search: A-Z; a-z; 1-9</li>
+    </ul>
+
+    <h3>Example:</h3>
+
+    <a href="https://carify.vercel.app/api/D-bl.json" target=__blank>URL: https://carify.vercel.app/api/D-bl.json</a>
+
+    <p>Output: {"{"}"title":"Balingen (Zollernalbkreis)","state":"Baden-Württemberg"{"}"}
+</main>
+
+<style>
+    main {
+        text-align: left;
+        margin-left: 1rem;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+</style>
