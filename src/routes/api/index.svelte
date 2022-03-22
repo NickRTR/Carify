554b28868b8cfe65568cb85fae9a01c069f53a40
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-03-22 21:38:14 +0100

    Fixed #5 again

--- src/routes/api/index.svelte
+++ src/routes/api/index.svelte
@@ -1,37 +1,39 @@
 <main>
-    <div class="api">
-        <h2>API</h2>
-
-        <h3>API URL:</h3>
-        <p style="text-decoration: underline;">https://carify.vercel.app/api/(country)-(search).json</p>
-
-        <ul>
-            <li>country: [d,a,de,at,germany,austria] (not case sensitive)</li>
-            <li>search: A-Z; a-z; 1-9</li>
-        </ul>
-
-        <h3>Example:</h3>
-
-        <a href="https://carify.vercel.app/api/D-bl.json" target=__blank>URL: https://carify.vercel.app/api/D-bl.json</a>
-
-        <p>Output: {"{"}"title":"Balingen (Zollernalbkreis)","state":"Baden-Württemberg"{"}"}
-    </div>
-    <hr>
-    <div class="reverseApi">
-        <h2>Reverse API</h2>
-
-        <h3>API URL:</h3>
-        <p style="text-decoration: underline;">https://carify.vercel.app/api/reverse/(country)-(search).json</p>
-
-        <ul>
-            <li>country: [d,a,de,at,germany,austria] (not case sensitive)</li>
-        </ul>
-
-        <h3>Example:</h3>
-
-        <a href="https://carify.vercel.app/api/reverse/d-Balingen (Zollernalbkreis)" target=__blank>URL: https://carify.vercel.app/api/reverse/d-Balingen (Zollernalbkreis)</a>
-
-        <p>Output: {"{"}"title":"Balingen (Zollernalbkreis)","state":"Baden-Württemberg","key":"BL"{"}"}
+    <div class="container">
+        <div class="api">
+            <h2>API</h2>
+    
+            <h3>API URL:</h3>
+            <p style="text-decoration: underline;">https://carify.vercel.app/api/(country)-(search).json</p>
+    
+            <ul>
+                <li>country: [d,a,de,at,germany,austria] (not case sensitive)</li>
+                <li>search: A-Z; a-z; 1-9</li>
+            </ul>
+    
+            <h3>Example:</h3>
+    
+            <a href="https://carify.vercel.app/api/D-bl.json" target=__blank>URL: https://carify.vercel.app/api/D-bl.json</a>
+    
+            <p>Output: {"{"}"title":"Balingen (Zollernalbkreis)","state":"Baden-Württemberg"{"}"}
+        </div>
+        <hr>
+        <div class="reverseApi">
+            <h2>Reverse API</h2>
+    
+            <h3>API URL:</h3>
+            <p style="text-decoration: underline;">https://carify.vercel.app/api/reverse/(country)-(search).json</p>
+    
+            <ul>
+                <li>country: [d,a,de,at,germany,austria] (not case sensitive)</li>
+            </ul>
+    
+            <h3>Example:</h3>
+    
+            <a href="https://carify.vercel.app/api/reverse/d-Balingen (Zollernalbkreis)" target=__blank>URL: https://carify.vercel.app/api/reverse/d-Balingen (Zollernalbkreis)</a>
+    
+            <p>Output: {"{"}"title":"Balingen (Zollernalbkreis)","state":"Baden-Württemberg","key":"BL"{"}"}
+        </div>
     </div>
 </main>
 
@@ -44,7 +46,11 @@
         text-align: left;
         max-width: 650px;
         margin: auto;
-        padding: 0 .5rem;
+        word-break: break-word;
+    }
+
+    .container {
+         margin: 0 .5rem;
     }
 
     a {
